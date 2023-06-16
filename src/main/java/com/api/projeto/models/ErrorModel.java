74f2b9df536fad4a534bb7e88d2ac6a0e7419c3f
Victor Muller <victormuller050@gmail.com>
2023-06-16 19:16:42 -0300

    ajustes

--- src/main/java/com/api/projeto/models/ErrorModel.java
+++ src/main/java/com/api/projeto/models/ErrorModel.java
@@ -11,30 +11,6 @@ public class ErrorModel {
         this.erro = erro.getMessage();
     }
 
-    public ErrorModel(String mensagem, String tipo) {
-        this.tipo = tipo;
-        this.mensagem = mensagem;
-        this.erro = "";
-    }
-
-    public ErrorModel(String mensagem) {
-        this.tipo = "";
-        this.mensagem = mensagem;
-        this.erro = "";
-    }
-
-    public ErrorModel() {
-        this.tipo = "Desconhecido";
-        this.mensagem = "Ops ... Ocorreu algum erro desconhecido";
-        this.erro = "";
-    }
-
-    public ErrorModel(Exception erro) {
-        this.tipo = "Desconhecido";
-        this.mensagem = "Ops ... Ocorreu algum erro desconhecido";
-        this.erro = erro.getMessage();
-    }
-
     public String getTipo() {
         return tipo;
     }
